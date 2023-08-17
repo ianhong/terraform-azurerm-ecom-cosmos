d4b8631c55f09b8d140b72124f51848592e729fc
Ian Hong <ianhong@gmail.com>
2023-08-16 22:23:46 -0700

    Add description

--- variable.tf
+++ variable.tf
@@ -1,13 +1,16 @@
 variable "db_name" {
     type = string
+    description = "OMS ECOM CosmosDB name"
 }
 
 variable "common_tags" {
     type = map(string)
     default = {}
+    description = "Common tags, Environment name"
 }
 
 variable "ip_range_filter" {
     type = list(string)
+    description = "IP range filter for CosmosDB"
 }
 
